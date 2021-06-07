d4cfe26e422bd41f157e902db35df7a020ec43d6
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-06-07 10:58:09 +0300

    make additions

--- app/models/news.py
+++ app/models/news.py
@@ -1,12 +1,12 @@
 class Movie:
     '''
-    Movie class to define Movie Objects
+    news class to define news Objects
     '''
 
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
-        self.id =id
-        self.title = title
-        self.overview = overview
-        self.poster = 'https://image.tmdb.org/t/p/w500/'+ poster
-        self.vote_average = vote_average
-        self.vote_count = vote_count
+    def __init__(self,author,description,time,url,image,title):
+      self.author = author
+      self.description = description
+      self.time = time
+      self.url = url
+      self.image = image
+      self.title = title   
